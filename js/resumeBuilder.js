477b5110cb02f2705cd8cee4700b8d78cf15ef4f
morningLemon <morningLemon@users.noreply@github.com>
2015-02-23 22:56:47 +0200

    added education JSON object of places of study

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -36,10 +36,30 @@
 	work.employer = "Helloccomputer";
 	work.years = 1.3;
 
-	var education = {};
-	education["name"] = "University of Cape Town";
-	education["years"] = "1996 - 1999";
-	education["city"] = "Cape Town";
+	// Education
+	var education = {
+		"schools": [
+			{
+				'name': "University of Cape Town",
+				'years': 3,
+				'degree': 'BA',
+				'major': ['English', 'Film']
+			},
+			{
+				'name': "WS\&L",
+				'years': 0.4,
+				'degree': 'Intro to Programmming'
+			},
+			{
+				'name': "City Varsity",
+				'years': 0.6,
+				'degree': 'Intro MIDI'
+			}
+		] 
+	};
+	// education["name"] = ";
+	// education["years"] = "1996 - 1999";
+	// education["city"] = "Cape Town";
 
 	$('#main').append(work["position"]);
 	$('#main').append(education.name);
