f7a262003a91d81a26dd225aa985c8472d06e642
Ian Summs <ian.summs@hellocomputer.com>
2015-02-23 09:17:36 +0200

    added jobs amd education

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,39 +5,59 @@
 		skills = [],
 		formattedName = HTMLheaderName.replace("%data%", name),
 		formattedRole = HTMLheaderRole.replace("%data%", role);
+
+	// Add skills to your skillset
+	skills.push('programming', 'dj \& music producer', 'UX');
+	console.log(skills);
 	
 	// Add formatted name and role to the header
 	pageHeader.prepend(formattedName, formattedRole);
 
 	// Populate your cotactInfo channels
-	var contactInfo = {
-		emailAddress: "morningLemon@users.noreply.github.com",
-		skype: 'ian.summs',
-		twitter: 'ian.summs',
-		github: "https://github.com/morningLemon"
+	var bio = {
+		"name": name,
+		"role": role,
+		"contacts": {
+			"emailAddress": "morningLemon@users.noreply.github.com",
+			"skype": 'ian.summs',
+			"twitter": '@ian.summs',
+			"github": "morningLemon",
+			"location": "Cape Town"
+		},
+		"welcomeMessage": "loren ipsoum dolor sit amet",
+		"skills": "skills",
+		"bioPic": "images/3293335.jpg"
+		
 	}
+
+	// Work
+	var work = {};
+	work.position = "Developer";
+	work.employer = "Helloccomputer";
+	work.years = 1.3;
+
+	var education = {};
+	education["name"] = "University of Cape Town";
+	education["years"] = "1996 - 1999";
+	education["city"] = "Cape Town";
+
+	$('#main').append(work["position"]);
+	$('#main').append(education.name);
+
 
 	// Todo formatt helper Objects to the contacts are clickable
 	var formattedEmailAddress = HTMLemail.replace("%data%", contactInfo.emailAddress),
 		formattedTwitterHandle = HTMLtwitter.replace("%data%", contactInfo.twitter),
 		formattedGitHub = HTMLgithub.replace("%data%", contactInfo.github);
 
-	$('#footerContacts').prepend(formattedEmailAddress, formattedTwitterHandle, formattedGitHub);
-
-	// Add skills to your skillset
-	skills.push('programming', 'dj \& music producer', 'UX');
-	console.log(skills);
-
-
-
-
+	// $('#footerContacts').prepend(formattedEmailAddress, formattedTwitterHandle, formattedGitHub);
 
 	// Create nwe object called bio
-	var bio = {
-		name: name,
-		role: role,
-		contactInfo: contactInfo,
-		pictureUrl: "images/3293335.jpg"
-	}
+	// var bio = {
+	// 	name: name,
+	// 	role: role,
+	// 	contactInfo: contactInfo,
+	// 	pictureUrl: "images/3293335.jpg"
+	// }
 	
 // }(window, document, jQuery));
